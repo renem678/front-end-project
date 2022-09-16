3df2a674f2d254e8c326e4465f19d5f39b32c376
Rene Marquez <r.marquez678@gmail.com>
2022-09-15 19:09:14 -0500

    able to fetch forcast data can not display

--- app.js
+++ app.js
@@ -18,15 +18,51 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp } = data.main;
         const { speed } = data.wind
-        console.log(name, icon, description, temp, speed)
+
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".weather-description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
-    }
+            ;
+    },
 };
+let forecast = {
+    apiKey: "ad0e698787f364b65657e5970560ef51",
+    fetchForecast: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+            + city
+            + "&cnt=5"
+            + "&units=imperial&appid="
+            + this.apiKey)
+
+            .then((response) => response.json())
+            .then((data) => this.getForecast(data));
+    },
+    search: function () {
+        this.fetchForecast(document.querySelector(".search-bar").value);
+    },
+    getForecast: function (data) {
+        forecastList = data.list;
+
+        for (var i = 0; i < forecastList; i++) {
+            const { description } = forecastList[i].weather.description;
+            const { temp } = forecastList[i].weather.main;
+            const { speed } = forecastList[i].weather.wind;
+
+            document.querySelector(".description").innerText = description;
+            data.list.append(".description")
+            document.querySelector(".tempature").innerText = temp + "°F";
+            data.list.append(".tempature")
+            document.querySelector(".windS").innerText = "Wind Speed: " + speed + " mph";
+            data.list.append(".windS")
+            console.log()
+        }
+    },
+};
+
+
 document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 })
@@ -34,5 +70,8 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     if (event.key == "Enter") {
         weather.search()
     }
+})
+document.querySelector(".forecast-button").addEventListener("click", function () {
+    forecast.search();
 })
 
